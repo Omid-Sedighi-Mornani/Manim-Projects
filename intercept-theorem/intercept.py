30bc5fd39aee1bc113ffb9ae96d18426ebdbaff6
omid.sed <osedighimornani@gmail.com>
2025-05-08 13:12:47 +0200

    intercept.py: Second Scene (VShape, Second Formula) done!

--- intercept-theorem/intercept.py
+++ intercept-theorem/intercept.py
@@ -25,6 +25,24 @@ class AbstractScene(Scene):
 
         self.parallel_formula = ExtendedMathTex("g", "\parallel", "h", color=BROWN)
 
+        self.first_formula = ExtendedMathTex(
+            "{ZA", "\over", "ZA'}", "=", "{ZB", "\over", "ZB'}", color=BROWN
+        ).to_edge(DOWN)
+
+        self.second_formula = ExtendedMathTex(
+            "{AB", "\over", "A'B'}", "=", "{ZB", "\over", "ZB'}", color=BROWN
+        ).to_edge(DOWN)
+
+        self.general_formula = ExtendedMathTex(
+            "{kurz", "\over", "lang}", "=", "{kurz", "\over", "lang}", color=BROWN
+        ).to_edge(DOWN)
+
+        for formula in VGroup(
+            self.first_formula, self.general_formula, self.second_formula
+        ):
+            formula[0, 4].set_color(OLIVE_GREEN)
+            formula[2, 6].set_color(FIRE_RED)
+
         frame = SVGMobject("assets/test.svg")
         frame.stretch_to_fit_height(config.frame_height)
         frame.stretch_to_fit_width(config.frame_width)
@@ -73,21 +91,13 @@ class IntroScene(AbstractScene):
             long_line_color=FIRE_RED,
         )
 
-        first_formula = ExtendedMathTex(
-            "{ZA", "\over", "ZA'}", "=", "{ZB", "\over", "ZB'}", color=BROWN
-        ).to_edge(DOWN)
+        # colors = [OLIVE_GREEN, FIRE_RED, OLIVE_GREEN, FIRE_RED]
+        # indices = [0, 2, 4, 6]
+        # for index, color in zip(indices, colors):
+        #     self.first_formula[index].set_color(color)
+        #     self.second_formula[index].set_color(color)
 
-        second_formula = ExtendedMathTex(
-            "{AB", "\over", "A'B'}", "=", "{ZB", "\over", "ZB'}", color=BROWN
-        )
-
-        colors = [OLIVE_GREEN, FIRE_RED, OLIVE_GREEN, FIRE_RED]
-        indices = [0, 2, 4, 6]
-        for index, color in zip(indices, colors):
-            first_formula[index].set_color(color)
-            second_formula[index].set_color(color)
-
-        VGroup(first_formula, second_formula, self.parallel_formula).arrange(
+        VGroup(self.first_formula, self.second_formula, self.parallel_formula).arrange(
             LEFT, buff=1
         ).to_edge(DOWN)
 
@@ -116,24 +126,24 @@ class IntroScene(AbstractScene):
             Create(x_shape.ZA),
             Create(v_shape.ZB),
             Create(x_shape.ZB),
-            *[Write(element) for element in first_formula[[0, 4]]],
+            *[Write(element) for element in self.first_formula[[0, 4]]],
         )
 
         self.play(
             Indicate(
                 VGroup(v_shape.ZA, v_shape.ZB, x_shape.ZA, x_shape.ZB), color=GREEN_A
             ),
-            Indicate(first_formula[[0, 4]], color=GREEN_A),
+            Indicate(self.first_formula[[0, 4]], color=GREEN_A),
         )
 
-        self.play(Write(first_formula[[1, 3, 5]]))
+        self.play(Write(self.first_formula[[1, 3, 5]]))
 
         self.play(
             Create(v_shape.ZA_dash),
             Create(x_shape.A_dash),
             Create(v_shape.ZB_dash),
             Create(x_shape.ZB_dash),
-            Write(first_formula[[2, 6]]),
+            Write(self.first_formula[[2, 6]]),
         )
 
         self.play(
@@ -143,23 +153,25 @@ class IntroScene(AbstractScene):
                 ),
                 color=RED_A,
             ),
-            Indicate(first_formula[[2, 6]], color=RED_A),
+            Indicate(self.first_formula[[2, 6]], color=RED_A),
         )
 
-        self.play(Create(v_shape.AB), Create(x_shape.AB), Write(second_formula[[0, 4]]))
+        self.play(
+            Create(v_shape.AB), Create(x_shape.AB), Write(self.second_formula[[0, 4]])
+        )
         self.play(
             Indicate(
                 VGroup(v_shape.AB, x_shape.AB, v_shape.ZB, x_shape.ZB), color=GREEN_A
             ),
-            Indicate(second_formula[[0, 4]], color=GREEN_A),
+            Indicate(self.second_formula[[0, 4]], color=GREEN_A),
         )
 
-        self.play(FadeIn(second_formula[1, 3, 5]))
+        self.play(FadeIn(self.second_formula[1, 3, 5]))
 
         self.play(
             Create(v_shape.AB_dash),
             Create(x_shape.AB_dash),
-            Write(second_formula[2, 6]),
+            Write(self.second_formula[2, 6]),
         )
         self.play(
             Indicate(
@@ -168,7 +180,7 @@ class IntroScene(AbstractScene):
                 ),
                 color=RED_A,
             ),
-            Indicate(second_formula[2, 6], color=RED_A),
+            Indicate(self.second_formula[2, 6], color=RED_A),
         )
 
         self.play(
@@ -244,21 +256,6 @@ class FirstScene(AbstractScene):
             v_shape.ray1, direction=DOWN, aligned_edge=RIGHT
         )
 
-        general_formula = ExtendedMathTex(
-            "{kurz", "\over", "lang}", "=", "{kurz", "\over", "lang}", color=BROWN
-        )
-
-        first_formula = ExtendedMathTex(
-            "{ZA", "\over", "ZA'}", "=", "{ZB", "\over", "ZB'}", color=BROWN
-        ).to_edge(DOWN)
-
-        for formula in VGroup(first_formula, general_formula):
-            formula[0, 4].set_color(OLIVE_GREEN)
-            formula[2, 6].set_color(FIRE_RED)
-
-        general_formula.to_edge(DOWN)
-        first_formula.move_to(general_formula)
-
         self.play(Create(v_shape.ZA))
         self.play(Write(short_label))
         self.wait()
@@ -275,17 +272,17 @@ class FirstScene(AbstractScene):
         self.wait()
 
         self.play(
-            TransformMatchingShapes(short_label, general_formula[0]),
-            FadeIn(general_formula[1]),
-            TransformMatchingShapes(long_label, general_formula[2]),
+            TransformMatchingShapes(short_label, self.general_formula[0]),
+            FadeIn(self.general_formula[1]),
+            TransformMatchingShapes(long_label, self.general_formula[2]),
         )
 
-        self.play(FadeIn(general_formula[3]))
+        self.play(FadeIn(self.general_formula[3]))
 
         self.play(
-            TransformMatchingShapes(s, general_formula[4]),
-            FadeIn(general_formula[5]),
-            TransformMatchingShapes(l, general_formula[6]),
+            TransformMatchingShapes(s, self.general_formula[4]),
+            FadeIn(self.general_formula[5]),
+            TransformMatchingShapes(l, self.general_formula[6]),
         )
 
         distances = ExtendedVGroup(
@@ -297,15 +294,15 @@ class FirstScene(AbstractScene):
 
         for i, j in zip(range(0, 7, 2), range(4)):
             self.play(Indicate(distances[j], scale_factor=1.1))
-            self.play(Transform(general_formula[i], first_formula[i]))
+            self.play(Transform(self.general_formula[i], self.first_formula[i]))
             self.wait()
 
-        self.play(ReplacementTransform(general_formula, first_formula))
+        self.play(ReplacementTransform(self.general_formula, self.first_formula))
 
         self.play(
             FadeOut(v_shape[:20]),
             self.table.animate.scale(0.1).move_to(ORIGIN),
-            first_formula.animate.scale(0.9).move_to(self.cell_locations[(2, 2)]),
+            self.first_formula.animate.scale(0.9).move_to(self.cell_locations[(2, 2)]),
             FadeTransform(title, self.overview_title),
             FadeOut(self.parallel_formula),
         )
@@ -329,14 +326,97 @@ class SecondScene(AbstractScene):
         previous_formula[0, 4].set_color(OLIVE_GREEN)
         previous_formula[2, 6].set_color(FIRE_RED)
         self.add(self.table, self.overview_title, previous_formula)
+
+        self.wait()
         self.play(
             self.table.animate.scale(10, about_point=self.cell_locations[(2, 3)]),
-            Transform(self.intercept_labels[1], title[:14]),
+            ReplacementTransform(self.intercept_labels[1].copy(), title[:14]),
             FadeOut(self.overview_title),
             FadeOut(previous_formula),
             FadeIn(title[14]),
-            Transform(self.shape_labels[0], title[15:]),
+            ReplacementTransform(self.shape_labels[0].copy(), title[15:]),
         )
+
+        v_shape = VShape().move_to(ORIGIN)
+        self.play(Create(v_shape[:2]))
+        self.wait()
+
+        self.play(Create(v_shape[2:4]), Write(v_shape[4:6]))
+
+        self.wait()
+
+        for i in range(6, 15, 2):
+            self.play(Write(v_shape[i]))
+
+        self.play(FadeIn(self.parallel_formula.to_edge(RIGHT)))
+
+        short_label = (
+            MathTex("kurz", color=OLIVE_GREEN)
+            .next_to(v_shape.ZA, direction=DOWN)
+            .shift(0.1 * LEFT)
+        )
+
+        long_label = MathTex("lang", color=FIRE_RED).next_to(
+            v_shape.ray1, direction=DOWN, aligned_edge=RIGHT
+        )
+
+        self.play(Create(v_shape.ZB), Write(short_label))
+        self.play(Create(v_shape.ZB_dash), Write(long_label))
+
+        self.play(
+            ReplacementTransform(v_shape.ZB.copy(), v_shape.AB),
+            ReplacementTransform(v_shape.ZB_dash.copy(), v_shape.AB_dash),
+            (s := short_label.copy()).animate.shift(1.5 * UP + 0.5 * LEFT),
+            (l := long_label.copy()).animate.shift(1.5 * UP),
+        )
+
+        self.wait()
+
+        self.play(
+            TransformMatchingShapes(short_label, self.general_formula[0]),
+            TransformMatchingShapes(long_label, self.general_formula[2]),
+            FadeIn(self.general_formula[1]),
+        )
+        self.play(FadeIn(self.general_formula[3]))
+
+        self.play(
+            TransformMatchingShapes(s, self.general_formula[4]),
+            TransformMatchingShapes(l, self.general_formula[6]),
+            FadeIn(self.general_formula[5]),
+        )
+
+        distances = ExtendedVGroup(
+            v_shape.AB, v_shape.AB_dash, v_shape.ZB, v_shape.ZB_dash
+        )
+
+        self.play(
+            FadeIn(
+                VGroup(
+                    v_shape.label_A,
+                    v_shape.label_B,
+                    v_shape.label_A_dash,
+                    v_shape.label_B_dash,
+                    v_shape.label_Z,
+                )
+            )
+        )
+
+        for i, j in zip(range(0, 7, 2), range(4)):
+            self.play(Indicate(distances[j], scale_factor=1.1))
+            self.play(Transform(self.general_formula[i], self.second_formula[i]))
+            self.wait()
+
+        self.play(ReplacementTransform(self.general_formula, self.second_formula))
+        previous_formula.scale(10).set_opacity(0)
+        self.play(
+            self.table.animate.scale(0.1).move_to(ORIGIN),
+            self.second_formula.animate.scale(0.9).move_to(self.cell_locations[(2, 3)]),
+            previous_formula.animate.scale(0.1).set_opacity(1),
+            FadeOut(v_shape),
+            FadeTransform(title, self.overview_title),
+            FadeOut(self.parallel_formula),
+        )
+
         self.wait(3)
 
 
